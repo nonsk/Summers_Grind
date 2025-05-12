fd985396b51cf1a5a875a5006278fc325d99077f
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-12 21:00:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob_sub(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        vector<int> dp(nums.size() + 1, -1);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.size() - 1];
+    }
+    int rob(vector<int>& nums) {
+        vector<int> v1(nums.begin() + 1, nums.end());
+        vector<int> v2(nums.begin(), nums.end() - 1);
+        return max(rob_sub(v1), rob_sub(v2));
+    }
+};
