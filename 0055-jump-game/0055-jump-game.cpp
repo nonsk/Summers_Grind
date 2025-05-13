bc8789e6f38a8a8db73973bfc05b0d58848145f1
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-13 09:07:13 +0530

    Time: 1414 ms (5%), Space: 54.9 MB (9.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -25,7 +25,23 @@ public:
         return mp[index];
     }
     bool canJump(vector<int>& nums) {
-        unordered_map<int, bool> mp;
-        return helper(0, nums, mp);
+        // unordered_map<int, bool> mp;
+        // return helper(0, nums, mp);
+        vector<int> dp(nums.size()+1, 0);
+        dp[0] = 1;
+        for(int i =0; i<nums.size(); i++ ){
+            if(dp[i]==0){
+                continue;
+            }
+            for(int j =0; j<=nums[i]; j++){
+                if(i+j>=dp.size()){
+                    continue;
+                }
+                dp[i+j] = 1;
+
+            }
+        }
+        return (dp[nums.size()-1])?true:false;
+
     }
 };
