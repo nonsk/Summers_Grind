5fc7759a129f59819a7ad7b43a7044f018f721bc
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-13 12:42:53 +0530

    Time: 3 ms (29.59%), Space: 16.3 MB (37.83%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -6,11 +6,11 @@ public:
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if(i==m-1&&j==n-1){
+                if (i == m - 1 && j == n - 1) {
                     dp[i][j] = grid[i][j];
                     continue;
                 }
-                dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
+                dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
             }
         }
         return dp[0][0];
