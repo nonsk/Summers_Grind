0b90b39948b8d2521df34b0ce2747c50da18ef7c
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-13 09:07:41 +0530

    Time: 534 ms (10.05%), Space: 54.8 MB (10.79%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
-    bool helper(int index, vector<int>& nums, unordered_map<int, bool>& mp) {
-        if (mp.find(index) != mp.end()) {
-            return mp[index];
-        }
-        if (index == nums.size() - 1) {
-            mp[index] = true;
-            return mp[index];
-        } else if (index >= nums.size()) {
-            mp[index] = false;
-            return mp[index];
-        } else if (nums[index] == 0) {
-            mp[index] = false;
-            return mp[index];
-        } else {
-            for (int i = 1; i <= nums[index]; i++) {
-                if (helper(index + i, nums, mp)) {
-                    mp[index] = true;
-                    return mp[index];
-                }
-            }
-        }
-        mp[index] = false;
-        return mp[index];
-    }
     bool canJump(vector<int>& nums) {
-        // unordered_map<int, bool> mp;
-        // return helper(0, nums, mp);
         vector<int> dp(nums.size()+1, 0);
         dp[0] = 1;
         for(int i =0; i<nums.size(); i++ ){
@@ -35,7 +9,7 @@ public:
             }
             for(int j =0; j<=nums[i]; j++){
                 if(i+j>=dp.size()){
-                    continue;
+                    break;
                 }
                 dp[i+j] = 1;
 
