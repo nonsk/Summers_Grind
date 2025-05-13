ac9e9c40a36d310b0a50eec974bbe21d37e9478e
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-13 19:28:14 +0530

    Time: 25 ms (4.11%), Space: 22.4 MB (54.54%) - LeetHub

---
+++ 3704-count-partitions-with-even-sum-difference/3704-count-partitions-with-even-sum-difference.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int sum_r = 0;
+        int sum_l = nums[0];
+        int ans = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            sum_r += nums[i];
+        }
+        // if ((sum_l - sum_r) % 2 == 0) {
+        //     ans++;
+        // }
+        for (int i = 1; i < nums.size(); i++) {
+            sum_l += nums[i];
+            sum_r -= nums[i];
+            cout<<sum_l<<" "<<sum_r<<endl;
+            if ((sum_l - sum_r) % 2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
