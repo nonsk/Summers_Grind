e76ae502e324821dc39e4512dfc3127a42c87331
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-14 16:32:45 +0530

    Time: 4 ms (36.92%), Space: 41.6 MB (15.77%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.cpp
+++ 1428-jump-game-iii/1428-jump-game-iii.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    bool helper(int i, vector<int>& arr, unordered_set<int>& visiting) {
-        if (visiting.count(i) > 0) {
+    bool helper(int i, vector<int>& arr, vector<int>& visiting) {
+        if (i >= arr.size() || i < 0 || visiting[i]) {
             return false;
         }
-        if (i >= arr.size() || i < 0) {
-            return false;
-        }
-        if(arr[i]==0){
+        if (arr[i] == 0) {
             return true;
         }
-        visiting.insert(i);
+        visiting[i] = 1;
         int ans = helper(i + arr[i], arr, visiting) ||
                   helper(i - arr[i], arr, visiting);
-        // visiting.erase(i); 
         return ans;
     }
 
     bool canReach(vector<int>& arr, int start) {
-        unordered_map<int, bool> dp;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] == 0) {
-                dp[i] = true;
-            }
-        }
-        unordered_set<int> visiting;
+        vector<int> visiting(arr.size()+1,0);
         return helper(start, arr, visiting);
     }
 };
