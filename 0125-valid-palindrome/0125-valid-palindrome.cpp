354da2fced0f69c7943129b4239490701734e5d1
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 18:39:18 +0530

    Time: 9 ms (7.35%), Space: 10.8 MB (5.99%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned;
+        for (auto it : s) {
+            if (isalnum(it)) {
+                cleaned += it;
+            }
+        }
+        //very_important
+        for (auto& c : cleaned) {
+            c = tolower(c);
+        }
+        string yo = cleaned;
+        reverse(cleaned.begin(), cleaned.end());
+        for(int i =0; i<cleaned.size(); i++){
+            if(cleaned[i]!=yo[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
