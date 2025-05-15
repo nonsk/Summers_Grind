d9ba3d8b9a2f900f5391dcddd47b3693c0bdd51f
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 11:36:58 +0530

    Time: 0 ms (100%), Space: 29.4 MB (96.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, int start, int end){
-        while(start<=end){
-            swap(nums[start], nums[end]);
-            start++;
-            end--;
-        }
-    }
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-        helper(nums,0, nums.size()-k-1);
-        helper(nums,nums.size()-k, nums.size()-1);
-        helper(nums,0, nums.size()-1);
+        int n = nums.size();
+        k = k % n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
