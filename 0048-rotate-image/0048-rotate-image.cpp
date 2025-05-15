b2218aac81e9976a92e56ff2d0c1458bf721a086
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 10:15:40 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.51%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -7,35 +7,15 @@ public:
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        cout << matrix[i][j] << " ";
-                    }
-                    cout << endl;
-                }
-                cout << endl << endl;
             }
         }
-        cout<<"n = "<<n<<endl;
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n/2; j++) {
+            for (int j = 0; j < n / 2; j++) {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = temp;
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        cout << matrix[i][j] << " ";
-                    }
-                    cout << endl;
-                }
-                cout << endl << endl;
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
             }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cout << matrix[i][j] << " ";
-            }
-            cout << endl;
         }
     }
 };
