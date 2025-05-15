31eeb715ce79cf26d0e05f11640f330865d29968
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 17:34:55 +0530

    Time: 0 ms (100%), Space: 28.1 MB (83.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int ans = nums[0];
-        int max_freq = 0;
-        for(auto it : nums){
-            if(mp.find(it)==mp.end()){
-                mp[it] = 1;
-            }
-            else{
-                mp[it] += 1;
-            }
-            if(mp[it]>max_freq){
-                max_freq = mp[it];
-                ans = it;
-            }
-        }
-        return ans;
+       sort(nums.begin(),nums.end());
+        return nums[nums.size() / 2];
     }
 };
