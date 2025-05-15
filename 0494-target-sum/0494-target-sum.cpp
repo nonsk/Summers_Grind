f36e6f406857f8c3046b5bc8bafe2f169e0dff2f
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 14:02:29 +0530

    Time: 931 ms (7.01%), Space: 11.7 MB (73.71%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int dfs(vector<int>& nums, int index, int sum, int target) {
+        if (index == nums.size()) {
+            return sum == target ? 1 : 0;
+        }
+        return dfs(nums, index + 1, sum + nums[index], target) +
+               dfs(nums, index + 1, sum - nums[index], target);
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return dfs(nums, 0, 0, target);
+    }
+};
