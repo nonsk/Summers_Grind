8de6b56ba5591457e067a5bb9a3e5a1a679b2f43
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 10:15:15 +0530

    Time: 1484 ms (1.14%), Space: 10.4 MB (8.6%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        cout << matrix[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                cout << endl << endl;
+            }
+        }
+        cout<<"n = "<<n<<endl;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        cout << matrix[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                cout << endl << endl;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
