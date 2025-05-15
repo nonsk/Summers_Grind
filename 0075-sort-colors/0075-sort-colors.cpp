1901bed8ecd41fdbcda516cb2ae85294cf0299c4
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 08:24:20 +0530

    Time: 1564 ms (0.9%), Space: 11.7 MB (11.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+    void swap(vector<int>& nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+            int z = 0;
+        for (int n = 0; n < 3; n++) {
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] == n) {
+                    swap(nums, i, z);
+                    z++;
+                }
+                for(auto it : nums){
+                    cout<<it<<" ";
+                }
+                cout<<endl;
+            }
+        }
     }
 };
