4f74fdad172fa8669a4d5ed255608e9c5d34e20f
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 17:34:24 +0530

    Time: 3 ms (29.39%), Space: 28.2 MB (58.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,11 +5,11 @@ public:
         int ans = nums[0];
         int max_freq = 0;
         for(auto it : nums){
-            if(mp.find(it)!=mp.end()){
-                mp[it] += 1;
+            if(mp.find(it)==mp.end()){
+                mp[it] = 1;
             }
             else{
-                mp[it] = 1;
+                mp[it] += 1;
             }
             if(mp[it]>max_freq){
                 max_freq = mp[it];
