6a4829a48d89f119f168081122a9f3a22eb1ccbc
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-15 19:20:51 +0530

    Time: 0 ms (100%), Space: 21.9 MB (14.3%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
         int sum = 0;
         for(auto it : nums){
