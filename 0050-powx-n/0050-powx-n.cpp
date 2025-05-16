85b9eff60c9be2944729965ba685227e4b6f0f37
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-16 09:24:26 +0530

    Time: 0 ms (100%), Space: 8.1 MB (100%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    double myPow(double x, long  n) {
-        
-        if(n==1 || x==1)return x;
-        else if(n==0) return 1;
-
-        if(n<0) return 1/myPow(x,-n);
-
-        if(n%2==0)
-            return myPow(x*x,n/2);
-        else 
-            return x*myPow(x*x,n/2);
+    double myPow(double x, long n) {
+        if (n == 1) {
+            return x;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return myPow(1 / x, (-1) * n);
+        }
+        return (n%2==1)?x*myPow(x*x,(n-1)/2):x*x*myPow(x*x,(n-1)/2);
     }
 };
