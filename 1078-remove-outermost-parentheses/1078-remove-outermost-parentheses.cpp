e3ccfe3bca1c35aaad29970eb3d17235ade77fe1
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-16 19:19:47 +0530

    Time: 3 ms (20.93%), Space: 9.1 MB (15.98%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int o = 0;
+        int c = 0;
+        // vector<string> v;
+        string temp = "";
+        string ans;
+        for(auto it : s){
+            if(it=='('){
+                o++;
+            }
+            else{
+                c++;
+            }
+            temp += it;
+            if(o==c && c!=0){
+                temp = temp.substr(1, temp.length() - 2);
+                ans += temp;
+                temp = "";
+            }
+        }
+        return ans;
+    }
+};
