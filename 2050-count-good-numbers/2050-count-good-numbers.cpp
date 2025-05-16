137383a7843b979f5dd52eb776a1b3b45791f25d
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-16 10:15:10 +0530

    Time: 0 ms (100%), Space: 7 MB (100%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.cpp
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int mod = 1e9+7;
-    
-    long long pow1(long long base, long long exp) {
-        long long ans = 1;
-        while (exp) {
-            if (exp % 2) { 
-                ans = (ans * base) % mod;
-            }
-            base = (base * base) % mod;
-            exp /= 2;
-        }
-        return ans;
-    }
-     
     int countGoodNumbers(long long n) {
-        long long even_place = (n + 1) / 2; 
-        long long odd_place = n / 2;       
-        
-        long long even = pow1(5, even_place) % mod;
-        long long odd = pow1(4, odd_place) % mod;
-        
-        return (even * odd) % mod;
+        cout<<n<<endl;
+        // 2,4,6,8,0
+        // 2,3,5,7
+        if (n == 1) {
+            return 5;
+        }
+        if(n%4==0){
+            long tempp = countGoodNumbers(n/2);
+            // cout<<n<<","<<tempp*tempp;
+            return tempp*tempp%(1000000007);
+        }
+        else if (n % 2 == 1) {
+            // cout<<n<<","<<(long) 5 * countGoodNumbers(n - 1)%(1000000007);
+            return (long) 5 * countGoodNumbers(n - 1)%(1000000007);
+        } else {
+            // cout<<n<<","<<(long) 4 * countGoodNumbers(n - 1)%(1000000007);
+            return (long) 4 * countGoodNumbers(n - 1)%(1000000007);
+        }
     }
 };
