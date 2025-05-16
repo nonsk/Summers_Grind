8f4ad41b0ec3775145875db0e1cd1c8eec88df09
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-16 16:13:56 +0530

    Time: 27 ms (6.09%), Space: 16 MB (9.68%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void recur(vector<int>& nums,int index, vector<int> temp, set<vector<int>> &ans){
+        for(auto it : temp){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        ans.insert(temp);
+        if(index>=nums.size()) return;
+        recur(nums,index+1, temp, ans);
+        temp.push_back(nums[index]);
+        recur(nums,index+1, temp, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        set<vector<int>> ans;
+        recur(nums,0,{},ans);
+        vector<vector<int>> new_ans;
+        for(auto it : ans){
+            new_ans.push_back(it);
+        }
+        return new_ans;
+    }
+};
