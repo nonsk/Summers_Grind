0e0ff92de30f12a4b8b658d2592889ca1a04c518
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-17 18:06:00 +0530

    Time: 0 ms (100%), Space: 12.4 MB (79%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int run = 0;
+        for(int i =0; i<nums.size(); i++){
+            run += nums[i];
+            ans.push_back(run);
+        }
+        return ans;
+    }
+};
