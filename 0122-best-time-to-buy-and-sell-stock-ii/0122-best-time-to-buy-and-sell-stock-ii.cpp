c3cdb3d0bb77672d69ec140a3f4183a981b4f5b3
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-17 11:21:28 +0530

    Time: 7 ms (16.97%), Space: 22.7 MB (5.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int p=0;
-        for(int i=1;i<prices.size();i++){
-            if(prices[i]>prices[i-1]){
-              p+=prices[i]-prices[i-1];
+    int recur(bool can, vector<int>& prices, int index, vector<vector<int>> &dp) {
+        if(dp[index][can]!=INT_MIN){
+            return dp[index][can];
+        }
+        if (index == prices.size() - 1) {
+            if (can) {
+                dp[index][1] = 0;
+                return 0;
+            } else {
+                dp[index][0] = prices[index];
+                return prices[index];
             }
         }
-        return p;
+        dp[index][1] = max(recur(false, prices, index + 1,dp) - prices[index],
+                       recur(true, prices, index + 1,dp));
+        dp[index][0] = max(recur(true, prices, index + 1,dp) + prices[index],
+                       recur(false, prices, index + 1,dp));
+        if (can) {
+            return dp[index][1];
+        } else {
+            return dp[index][0];
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp (prices.size()+1,vector<int>(2,INT_MIN));
+        return recur(true, prices, 0, dp);
     }
 };
