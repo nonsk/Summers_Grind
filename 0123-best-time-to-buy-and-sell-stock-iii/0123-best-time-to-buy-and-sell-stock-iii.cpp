c54f81a0cff4634f28c1752a7b92bec062eecbdd
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-17 11:56:03 +0530

    Time: 376 ms (48.89%), Space: 211.5 MB (35.88%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
-    int recur(bool can, vector<int>& prices, int index, vector<vector<vector<int>>> &dp, int l) {
-
-        if(dp[index][can][l]!=INT_MIN){
-            return dp[index][can][l];
-        }
-        if(l<=0){
-            return 0;
-         }
-        if (index == prices.size() - 1) {
-            if (can) {
-                dp[index][1][l] = 0;
-                return 0;
-            } else {
-                dp[index][0][l] = prices[index];
-                return prices[index];
+    int maxProfit(vector<int>& prices) {
+        int k = 2;
+        vector<vector<vector<int>>> dp(prices.size() + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+        for (int index = prices.size() - 1; index >= 0; index--) {
+            for (int cap = 1; cap <= k; cap++) {
+                dp[index][1][cap] = max(dp[index + 1][0][cap] - prices[index], dp[index + 1][1][cap]);
+                dp[index][0][cap] = max(dp[index + 1][1][cap - 1] + prices[index], dp[index + 1][0][cap]);
             }
         }
-        dp[index][1][l] = max(recur(false, prices, index + 1,dp, l) - prices[index],
-                       recur(true, prices, index + 1,dp, l));
-        dp[index][0][l] = max(recur(true, prices, index + 1,dp, l-1) + prices[index],
-                       recur(false, prices, index + 1,dp, l));
-        if (can) {
-            return dp[index][1][l];
-        } else {
-            return dp[index][0][l];
-        }
-    }
-    int maxProfit(vector<int>& prices) {
-        int l =2;
-        vector<vector<vector<int>>> dp (prices.size()+1,vector<vector<int>>(2,vector<int>(l+1,INT_MIN)));
-        return recur(true, prices, 0, dp, l);
+        return dp[0][1][k];
     }
 };
