d9ee4064d89dcdb303a10a5568c55f57abbd98f5
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-17 11:32:12 +0530

    Time: 15 ms (32.82%), Space: 18.5 MB (18.36%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int recur(bool can, vector<int>& prices, int index, vector<vector<vector<int>>> &dp, int l) {
+
+        if(dp[index][can][l]!=INT_MIN){
+            return dp[index][can][l];
+        }
+        if(l<=0){
+            return 0;
+         }
+        if (index == prices.size() - 1) {
+            if (can) {
+                dp[index][1][l] = 0;
+                return 0;
+            } else {
+                dp[index][0][l] = prices[index];
+                return prices[index];
+            }
+        }
+        dp[index][1][l] = max(recur(false, prices, index + 1,dp, l) - prices[index],
+                       recur(true, prices, index + 1,dp, l));
+        dp[index][0][l] = max(recur(true, prices, index + 1,dp, l-1) + prices[index],
+                       recur(false, prices, index + 1,dp, l));
+        if (can) {
+            return dp[index][1][l];
+        } else {
+            return dp[index][0][l];
+        }
+    }
+    int maxProfit(int k,vector<int>& prices) {
+        int l =k;
+        vector<vector<vector<int>>> dp (prices.size()+1,vector<vector<int>>(2,vector<int>(l+1,INT_MIN)));
+        return recur(true, prices, 0, dp, l);
+    }
+};
