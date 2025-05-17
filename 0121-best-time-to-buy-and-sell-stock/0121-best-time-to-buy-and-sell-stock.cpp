4011b12e3203f35acec2e9e12300836db10f2edd
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-17 09:14:47 +0530

    Time: 1 ms (37.37%), Space: 101.3 MB (5.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -6,7 +6,6 @@ public:
         for(int i =0; i<prices.size(); i++){
             m = min(m,prices[i]);
             mini[i] = m;
-            // cout<<m<<" ";
         }
         int profit = 0;
         for(int i =prices.size()-1; i>=1; i--){
