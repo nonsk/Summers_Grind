38ed64910f83ccbada783b3b31c81dcb94104a8c
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-18 18:32:33 +0530

    Time: 27 ms (34.09%), Space: 37.5 MB (55.17%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,9 +4,9 @@ public:
         queue<pair<int, int>> q;
         vector<vector<int>> ans(mat.size(),
                                 vector<int>(mat[0].size(), INT_MAX));
-        vector<vector<int>> vis(mat.size() + 1,
-                                vector<int>(mat[0].size() + 1, 0));
-
+        vector<vector<int>> vis(mat.size(),
+                                vector<int>(mat[0].size(), 0));
+         
         for (int i = 0; i < mat.size(); i++) {
             for (int j = 0; j < mat[0].size(); j++) {
                 if (mat[i][j] == 0) {
@@ -16,25 +16,24 @@ public:
                 }
             }
         }
+        
+        vector<pair<int, int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
         while (!q.empty()) {
             pair<int, int> node = q.front();
             q.pop();
             int i = node.first;
             int j = node.second;
-            vector<pair<int, int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-            int m = INT_MAX;
+            
             for (auto [dx, dy] : dir) {
                 int ni = i + dx, nj = j + dy;
                 if (ni >= 0 && ni < mat.size() && nj >= 0 &&
-                    nj < mat[0].size()) {
-                    m = min(m, ans[ni][nj]);
-                    if (!vis[ni][nj]) {
-                        q.push({ni, nj});
-                        vis[ni][nj] = 1;
-                    }
+                    nj < mat[0].size() && !vis[ni][nj]) {
+                    ans[ni][nj] = ans[i][j] + 1;
+                    q.push({ni, nj});
+                    vis[ni][nj] = 1;
                 }
             }
-            if(ans[i][j]==INT_MAX){ans[i][j] = m + 1;}
         }
         return ans;
     }
