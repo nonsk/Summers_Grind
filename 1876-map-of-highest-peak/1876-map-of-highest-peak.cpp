4904f23c28496c2a064798018589726ddf8df5f9
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-18 18:47:41 +0530

    Time: 100 ms (21.81%), Space: 120.5 MB (42.76%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        queue<pair<int, int>> q;
+        vector<vector<int>> ans(isWater.size(),
+                                vector<int>(isWater[0].size(), INT_MAX));
+        vector<vector<int>> vis(isWater.size(),
+                                vector<int>(isWater[0].size(), 0));
+         
+        for (int i = 0; i < isWater.size(); i++) {
+            for (int j = 0; j < isWater[0].size(); j++) {
+                if (isWater[i][j] == 1) {  // Changed from 0 to 1 - water cells have value 1
+                    ans[i][j] = 0;
+                    q.push({i, j});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+        
+        vector<pair<int, int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        
+        while (!q.empty()) {
+            pair<int, int> node = q.front();
+            q.pop();
+            int i = node.first;
+            int j = node.second;
+            
+            for (auto [dx, dy] : dir) {
+                int ni = i + dx, nj = j + dy;
+                if (ni >= 0 && ni < isWater.size() && nj >= 0 &&
+                    nj < isWater[0].size() && !vis[ni][nj]) {
+                    ans[ni][nj] = ans[i][j] + 1;
+                    q.push({ni, nj});
+                    vis[ni][nj] = 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
