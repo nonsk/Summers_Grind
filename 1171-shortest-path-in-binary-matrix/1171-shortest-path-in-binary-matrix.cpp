bfa906f9ad29fd087e491dae29db1b3aaeb7ae48
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-20 18:52:26 +0530

    Time: 26 ms (42.69%), Space: 26.3 MB (34.79%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -2,57 +2,33 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
-            return -1;
-        
-        map<pair<int, int>, vector<pair<int, int>>> mp;
-        vector<pair<int, int>> dir = {{0, 1},  {1, 0}, {0, -1}, {-1, 0},
-                                      {-1, 1}, {1, 1}, {1, -1}, {-1, -1}};
-        map<pair<int, int>, bool> vis;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    vis[{i, j}] = false;
-                    for (auto [dx, dy] : dir) {
-                        int ndx = dx + i;
-                        int ndy = dy + j;
-                        if (ndx >= 0 && ndx < n && ndy >= 0 && ndy < n &&
-                            grid[ndx][ndy] == 0) {
-                            mp[{i, j}].push_back({ndx, ndy});
-                        }
+        const int INF = 1e9;
+        vector<vector<int>> ans(n,vector<int>(n,INF));
+        queue<pair<pair<int,int>,int>> q;
+        if(grid[0][0] == 1)return -1;
+        q.push({{0,0},1});
+        ans[0][0] = 1;
+        vector<pair<int,int>> d = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
+        while(!q.empty()){
+            auto [p,dist] = q.front();
+            int x = p.first;
+            int y = p.second;
+            q.pop();
+
+            for(int i = 0;i < 8;i++){
+                int nx = x + d[i].first;
+                int ny = y + d[i].second;
+                if(nx >= 0 && nx < n && ny >= 0 && ny < n){
+                    if(grid[nx][ny] != 1 && dist + 1 <ans[nx][ny]){
+                        ans[nx][ny] = 1 + dist;
+                        q.push({{nx,ny},1 + dist});
                     }
                 }
             }
         }
-        
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        vis[{0, 0}] = true;
-        int lvl = 1; 
-        
-        while (!q.empty()) {
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                pair<int, int> u = q.front();
-                q.pop();
-                
-                
-                if (u.first == n-1 && u.second == n-1) {
-                    return lvl;
-                }
-                
-                for (auto [iu, ju] : mp[u]) {
-                    if (!vis[{iu, ju}]) {
-                        q.push({iu, ju});
-                        vis[{iu, ju}] = true;
-                    }
-                }
-            }
-            lvl++; 
-        }
-        
-        return -1; 
+
+
+        return ans[n - 1][n - 1] != INF?ans[n - 1][n - 1]:-1;
+
     }
 };
