102f0c52c30615a8eeb17c4e5421cd22753f15f9
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-25 18:07:33 +0530

    Time: 3 ms (73.16%), Space: 12.2 MB (67.71%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        int num = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'I') {
+                if (s[i + 1] == 'V') {
+                    num += 4;
+                    i++;
+                } else if (s[i + 1] == 'X') {
+                    num += 9;
+                    i++;
+                } else {
+                    num += 1;
+                }
+            } else if (s[i] == 'X') {
+                if (s[i + 1] == 'L') {
+                    num += 40;
+                    i++;
+                } else if (s[i + 1] == 'C') {
+                    num += 90;
+                    i++;
+                } else {
+                    num += 10;
+                }
+            } else if (s[i] == 'C') {
+                if (s[i + 1] == 'D') {
+                    num += 400;
+                    i++;
+                } else if (s[i + 1] == 'M') {
+                    num += 900;
+                    i++;
+                } else {
+                    num += 100;
+                }
+            } else if (s[i] == 'V') {
+                num += 5;
+            } else if (s[i] == 'L') {
+                num += 50;
+            } else if (s[i] == 'D') {
+                num += 500;
+            } else if (s[i] == 'M') {
+                num += 1000;
+            }
+        }
+        return num;
+    }
+};
