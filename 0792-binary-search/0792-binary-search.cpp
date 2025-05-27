3741a3e0690de8e4cc89058fa59bacee23886f04
nonsk <132883762+nonsk@users.noreply.github.com>
2025-05-28 00:06:14 +0530

    Time: 2 ms (2.62%), Space: 31.4 MB (9.41%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        int mid = (l+r)/2;
+        while(l<=r){
+            cout<<l<<","<<r<<endl;
+            if(target==nums[mid]){
+                return mid;
+            }
+            if(target<nums[mid]){
+                r = mid-1;
+            }
+            if(target>nums[mid]){
+                l = mid+1;
+            }
+                mid = (l+r)/2;
+        }
+        return -1;
+    }
+};
