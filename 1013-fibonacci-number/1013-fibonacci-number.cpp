61d1e7917f8599596379fd80ba020b758debb94f
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-14 01:28:13 +0530

    Time: 0 ms (100%), Space: 7.9 MB (21.02%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-        int curr;
-        int prev=0;
-        int prev2=1;
-        for (int i = 0; i < n; i++) {
-            curr = prev+prev2;
-            prev2 = prev;
-            prev = curr;
-        }
-        return curr;
+        if(n<=1)return n;
+        return fib(n-1) + fib(n-2);
     }
 };
 auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
