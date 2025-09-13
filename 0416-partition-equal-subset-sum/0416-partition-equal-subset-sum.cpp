7cb3939ba9213f5993081296f4cba1130c169db7
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-14 01:40:32 +0530

    Time: 261 ms (6.05%), Space: 204.1 MB (8.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int helper(int i, int sum, vector<int>& nums, int target, vector<vector<int>>& dp) {
-        if (i == nums.size()) {
-            if (sum == target / 2) {
-                return 1;
-            }
-            return 0;
+    int tot = 0;
+    int n;
+    vector<vector<int>> dp;
+    bool recur(int index, int sum, vector<int>& nums) {
+        if (sum == tot / 2) {
+            return true;
         }
-        if (dp[i][sum] != -1) {
-            return dp[i][sum];
-        }
-        int skip = helper(i + 1, sum, nums, target, dp);
-        int take = helper(i + 1, sum + nums[i], nums, target, dp);
-        dp[i][sum] = skip || take;
-        return dp[i][sum];
-    }
-    bool canPartition(vector<int>& nums) {
-        int target = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            target += nums[i];
-        }
-        if (target % 2 == 1) {
+        if (index == n) {
             return false;
         }
-        vector<vector<int>> dp(nums.size() + 1, vector<int>(target+1, -1));
-        return helper(0, 0, nums, target, dp);
+        if (dp[index][sum] != -1)
+            return dp[index][sum];
+        bool take = recur(index + 1, sum, nums);
+        bool notTake = recur(index + 1, sum + nums[index], nums);
+        dp[index][sum] = take || notTake;
+        return dp[index][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        n = nums.size();
+        for (int i = 0; i < n; i++) {
+            tot += nums[i];
+        }
+        if (tot % 2 == 1)
+            return false;
+        dp.resize(nums.size(), vector<int>(tot, -1));
+        return recur(0, 0, nums);
     }
 };
