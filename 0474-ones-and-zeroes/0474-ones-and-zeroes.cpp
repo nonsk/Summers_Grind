a9a74efbaaf9cef11b6de493ae740b4d35ff922b
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-14 01:15:11 +0530

    Time: 147 ms (53.96%), Space: 104.8 MB (45.35%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int size;
+    vector<pair<int, int>> count;
+    vector<vector<vector<int>>> dp;
+    void f1(vector<string>& strs) {
+        for (auto it : strs) {
+            int one = 0;
+            int zero = 0;
+            for (auto ch : it) {
+                if (ch == '0') {
+                    zero++;
+                } else {
+                    one++;
+                }
+            }
+            count.push_back({zero, one});
+        }
+    }
+    int recur(int index, int onesr, int zerosr) {
+        if (index == size)
+            return 0;
+        if(dp[index][onesr][zerosr]!=-1)return dp[index][onesr][zerosr];
+        int ans = 0;
+        ans = max(ans, recur(index + 1, onesr, zerosr));
+        int nzerosr = zerosr-count[index].first;
+        int nonesr = onesr-count[index].second;
+        if (nzerosr >= 0 && nonesr >= 0) {
+            ans = max(ans,1+recur(index + 1, nonesr, nzerosr));
+        }
+        dp[index][onesr][zerosr] = ans;
+        return ans;
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        size = strs.size();
+        f1(strs);
+        dp.assign(strs.size()+1,vector<vector<int>>(n+1,vector<int>(m+1,-1)));
+        return recur(0, n, m);
+    }
+};
