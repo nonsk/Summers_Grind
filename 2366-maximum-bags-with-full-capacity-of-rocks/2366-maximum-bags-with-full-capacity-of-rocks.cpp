966c0f08299ca0d2bb73fdac1973da834ec14e98
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-15 20:03:22 +0530

    Time: 1282 ms (5.33%), Space: 91.1 MB (78.38%) - LeetHub

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = capacity.size();
+        vector<int> fill(capacity.size(), -1);
+        for(int i = 0; i<n; i++){
+            fill[i] = capacity[i]-rocks[i];
+        }
+        sort(fill.begin(), fill.end());
+        int ans = 0;
+        int i = 0;
+        while(i<n && additionalRocks>=0){
+            additionalRocks -= fill[i];
+            if(additionalRocks<0)break;
+            cout<<additionalRocks<<' ';
+            ans++;
+            i++;
+        }
+        cout<<endl;
+        for(int i =0; i<n; i++){
+            cout<<fill[i]<<' ';
+        }
+        // ans--;
+        return ans;
+
+
+    }
+};
