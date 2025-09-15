5ce041ee39dd6c5ba34365f8163f3df26f2fc8b9
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-15 20:09:11 +0530

    Time: 20 ms (44.95%), Space: 88.8 MB (85.43%) - LeetHub

--- 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/2366-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -8,7 +8,6 @@ public:
         }
         sort(fill, fill + n);
         int ans = 0;
-        int i = 0;
         for(int i = 0; i<n; i++){
             additionalRocks -= fill[i];
             if(additionalRocks<0)break;
