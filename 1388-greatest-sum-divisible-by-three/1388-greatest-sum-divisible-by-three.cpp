7efe746f9e7fa347478984c080b6e05c405a6278
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-15 20:50:37 +0530

    Time: 39 ms (42.11%), Space: 46.1 MB (16.35%) - LeetHub

--- 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    map<pair<int,int>, int> mp;
-    
-    int recur(int index, int targetRem, vector<int>& nums) {
-        if (index == -1) {
-            return targetRem == 0 ? 0 : INT_MIN;
+    vector<vector<int>> dp;
+    int recur(int index, int rem,vector<int>& nums){
+        if(index==nums.size()){
+            if(rem == 0){
+                return 0;
+            }
+            else{
+                return (1e9+7)*(-1);
+            }
         }
-        
-        if (mp.find({index, targetRem}) != mp.end()) {
-            return mp[{index, targetRem}];
-        }
-        int newTargetRem = (targetRem - nums[index] % 3 + 3) % 3;
-        int include = nums[index] + recur(index - 1, newTargetRem, nums);
-        int exclude = recur(index - 1, targetRem, nums);
-        
-        return mp[{index, targetRem}] = max(include, exclude);
+        if(dp[index][rem]!=-1)return dp[index][rem];
+        // take
+        int take = recur(index+1, ((nums[index]+rem)%3+3)%3,nums) + nums[index];
+        int notake = recur(index+1,rem,nums);
+        dp[index][rem] = max(take, notake);
+        return dp[index][rem];
     }
-    
     int maxSumDivThree(vector<int>& nums) {
-        int n = nums.size();
-        return recur(n - 1, 0, nums);
+        dp.assign(nums.size()+1, vector<int>(3,-1));
+        return recur(0,0, nums);
     }
 };
