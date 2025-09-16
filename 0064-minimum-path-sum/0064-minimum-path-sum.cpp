9ba59a91fa94f6a5daab5e39a95ebd0064167441
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-16 15:47:17 +0530

    Time: 6 ms (6.54%), Space: 16.4 MB (41.13%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -5,10 +5,8 @@ public:
     int recur(int i, int j, vector<vector<int>>& grid){
         if(i<0 || j<0)return 1e9;
         if(dp[i][j]!=-1)return dp[i][j];
-
         int up = recur(i-1,j,grid);
         int left = recur(i,j-1,grid);
-
         dp[i][j] = min(up,left) + grid[i][j];
         return dp[i][j];
     }
