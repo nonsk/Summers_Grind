e75b7ee1764f6e4923e0a6ec419f63dd00806949
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-16 21:26:36 +0530

    Time: 0 ms (100%), Space: 31.2 MB (16.06%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -24,5 +24,4 @@ public:
     }
 };
 
-// auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
-auto init = atexit([]() { ofstream("display_memory.txt") << "0"; });
+auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
