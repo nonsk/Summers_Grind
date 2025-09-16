9770ab12cd8ceafa849dd34ef2cc9acbc8fc0773
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-16 21:24:34 +0530

    Time: 1077 ms (5%), Space: 31 MB (61.02%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n,m;
+    vector<vector<int>> dp;
+    int recur(int i, int j, vector<vector<char>>& matrix){
+        if(i==0 || j==0 || matrix[i][j] == '0'){
+            return matrix[i][j]-'0';
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        dp[i][j] =  min({recur(i-1,j,matrix),recur(i-1,j-1,matrix),recur(i,j-1,matrix)}) + 1;
+        return dp[i][j];
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        n = matrix.size();
+        m = matrix[0].size();
+        dp.assign(n, vector<int>(m,-1));
+        int ans = 0;
+        for(int i =0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                ans = max(ans, recur(i,j,matrix));
+            }
+        }
+        for(int i =0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                cout<<dp[i][j]<<' ';
+            }
+            cout<<endl;
+        }
+        return ans*ans;
+    }
+};
