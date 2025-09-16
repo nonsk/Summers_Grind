2716891d3c2ef747567d5fa15d11f8fc900046c0
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-16 14:46:56 +0530

    Time: 3 ms (9.48%), Space: 17.7 MB (31.03%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int helper(vector<int>& cost, int n, vector<int>& dp) {
-        if (n < 0) {
+    vector<int> dp;
+    int n;
+    int recur(int index, vector<int>& cost) {
+        if (index == n)
             return 0;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        if (n == cost.size()) {
-            dp[n] = min(helper(cost, n - 1, dp), helper(cost, n - 2, dp));
-        } else {
-            dp[n] =
-                min(helper(cost, n - 1, dp), helper(cost, n - 2, dp)) + cost[n];
-        }
-        return dp[n];
+        if (index > n)
+            return 1e9;
+        if(dp[index]!=-1)return dp[index];
+        dp[index] =  min(recur(index + 1, cost), recur(index + 2, cost)) +
+               cost[index];
+               return dp[index];
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n + 1, -1);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for(int i =2; i<n; i++){
-            dp[i] = min(dp[i-1], dp[i-2]) + cost[i];
-            
-        }
-        return min(dp[n-2],dp[n-1]);
+        n = cost.size();
+        dp.assign(n+1,-1);
+        return min(recur(0,cost), recur(1,cost));
     }
 };
