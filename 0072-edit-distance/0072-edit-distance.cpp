39b6fd70d3bc181fa563b7458ed6eaeabed22321
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-17 00:33:34 +0530

    Time: 7 ms (59.81%), Space: 14.2 MB (24.93%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int n,m;
+    int recur(int i, int j, string &word1, string &word2){
+        if(i==n)return m-j;
+        if(j==m)return n-i;
+        // transition
+
+        if(word1[i]==word2[j]) return recur(i+1,j+1,word1,word2);
+        if(dp[i][j]!=-1)return dp[i][j];
+        //insert
+        int ins = recur(i,j+1,word1,word2)+1;
+        //deletion
+        int del = recur(i+1,j,word1,word2)+1;
+        //replace
+        int rep = recur(i+1,j+1,word1,word2)+1;
+
+        return dp[i][j] = min({ins,del,rep});
+    }
+    int minDistance(string word1, string word2) {
+        
+        n = word1.size();
+        m = word2.size();
+        dp.assign(n,vector<int>(m,-1));
+        return recur(0,0,word1,word2);
+    }
+};
