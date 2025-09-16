61bc9a8623793a7cc4207e1348f2f1372e4c3810
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-16 15:47:11 +0530

    Time: 3 ms (32.01%), Space: 16.4 MB (14.88%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int n,m;
+    int recur(int i, int j, vector<vector<int>>& grid){
+        if(i<0 || j<0)return 1e9;
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        int up = recur(i-1,j,grid);
+        int left = recur(i,j-1,grid);
+
+        dp[i][j] = min(up,left) + grid[i][j];
+        return dp[i][j];
+    }
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (i == m - 1 && j == n - 1) {
-                    dp[i][j] = grid[i][j];
-                    continue;
-                }
-                dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
-            }
-        }
-        return dp[0][0];
+        n = grid.size();
+        m = grid[0].size();
+        dp.assign(n+1,vector<int>(m+1,-1));
+        dp[0][0] = grid[0][0];
+        return recur(n-1, m-1, grid);
     }
 };
