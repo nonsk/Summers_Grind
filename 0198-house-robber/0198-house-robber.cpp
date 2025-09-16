ff03cb72829575feefae346bdbaad3185ef388d0
nonsk <132883762+nonsk@users.noreply.github.com>
2025-09-16 14:31:26 +0530

    Time: 1 ms (8.98%), Space: 10.5 MB (64.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
+    int n;
+    vector<int> dp;
+    int recur(int index, vector<int>& nums) {
+        if (index >= n)
+            return 0;
+        if (dp[index] != -1)
+            return dp[index];
+        dp[index] =
+            max(recur(index + 1, nums), recur(index + 2, nums) + nums[index]);
+        return dp[index];
+    }
     int rob(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
-        vector<int> dp(nums.size() + 1, -1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[1], nums[0]);
-        for (int i = 2; i < nums.size(); i++) {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-        return dp[nums.size()-1];
+        n = nums.size();
+        dp.assign(n + 1, -1);
+        return recur(0, nums);
     }
 };
